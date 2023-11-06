fab9e48d75905d0cb826ced4de562252da2611c8
Michael Krysmalski <mk@bitsandlikes.de>
2023-11-06 16:49:38 +0100

    mongo api connection

--- apps/cloud-file-storage-api/src/app.module.ts
+++ apps/cloud-file-storage-api/src/app.module.ts
@@ -3,8 +3,15 @@ import { FileModule } from './file/file.module';
 import { DirModule } from './dir/dir.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [FileModule, DirModule, AuthModule, UserModule],
+  imports: [
+    FileModule, 
+    DirModule, 
+    AuthModule,
+    UserModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
 })
 export class AppModule {}
